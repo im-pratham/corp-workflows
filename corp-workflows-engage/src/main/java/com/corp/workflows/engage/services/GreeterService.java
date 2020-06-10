09173d7b96a374026c53d4801a582c49f8955fc2
Jorge Mora Giménez <jorge@ge.org.es>
2020-06-10 09:58:31 +0200

    GreeterService

---
+++ corp-workflows-engage/src/main/java/com/corp/workflows/engage/services/GreeterService.java
@@ -0,0 +1,21 @@
+package com.corp.workflows.engage.services;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.task.service.delegate.DelegateTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreeterService {
+
+    private static final Logger logger = LoggerFactory.getLogger(GreeterService.class);
+
+    public void greet(String greeter, String personToBeGreeted, DelegateExecution execution) {
+        //You can access the execution context as you would do with with a JavaDelegate
+        execution.setVariable("newVar", "newVarValue");
+        logger.info(greeter + " says hello to " + personToBeGreeted);
+    }
+    
+}
